86da58a2af31aaf6c51678ed4198fe7abbacefbf
ArjunGokul <arjuns.gokul@gmail.com>
2026-01-12 17:30:49 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,3 +1,15 @@
-variable "region" {}
-variable "instance_type" {}
-variable "ami" {}
+variable "region" {
+ type = string
+}
+variable "instance_type" {
+type = string
+}
+variable "ami" {
+type = string
+}
+variable "access_key" {
+type = string
+}
+variable "secret_access_key" {
+type = string
+}
