a64c6f8e36c8c02f9f395cf953b31cd266cbbe8e
ArjunGokul <arjuns.gokul@gmail.com>
2026-01-21 12:26:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,29 @@
+resource "tls_private_key" "my_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+resource "local_file" "private_key_file" {
+  content         = tls_private_key.my_key.private_key_pem
+  filename        = "~/myprivate.key"
+  file_permission = "0400"
+}
+
+resource "local_file" "public_key_file" {
+  content  = tls_private_key.my_key.public_key_openssh
+  filename = "~/mypublic.pem"
+}
+
+resource "aws_key_pair" "my-keypair" {
+  key_name   = "my-keypair"
+  public_key = tls_private_key.my_key.public_key_openssh
+}
+data "aws_vpc" "default-vpc" {
+  id = "vpc-0ce30347b3cd6bab8"
+}
+
 resource "aws_instance" "nginx-server" {
   instance_type = var.instance_type
+  key_name = aws_key_pair.my-keypair.key_name
   ami = var.ami
   tags = {
    Name = "nginx-server-01"
